75292c8fbf1714d4ead9e42b0a5ba4a3bf389c0a
Juan Ceballos Medina <juanceballosmedina@MacBook-Air-de-Juan.local>
2025-10-03 18:39:33 -0600

    feature: addition of the dockerized backend api

---
+++ mandarinplayer-api/mandarinplayer/src/main/java/com/cleanteam/mandarinplayer/MandarinplayerApplication.java
@@ -0,0 +1,13 @@
+package com.cleanteam.mandarinplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandarinplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandarinplayerApplication.class, args);
+	}
+
+}
---
+++ mandarinplayer-api/mandarinplayer/src/test/java/com/cleanteam/mandarinplayer/MandarinplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanteam.mandarinplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandarinplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
